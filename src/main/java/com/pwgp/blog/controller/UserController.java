ec6769a64341fa08279cd2bf0f35c6b12c2c5f70
= <=>
2023-11-25 21:56:08 +0100

    return response entity instead of string

--- src/main/java/com/pwgp/blog/controller/UserController.java
+++ src/main/java/com/pwgp/blog/controller/UserController.java
@@ -1,29 +1,25 @@
 package com.pwgp.blog.controller;
 
-import com.pwgp.blog.entity.User;
+import com.pwgp.blog.dto.user.UserResponse;
 import com.pwgp.blog.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserMapper userMapper;
 
-    @GetMapping("/users/{username}")
-    public String getUser(@PathVariable("username") String username, Model model) {
-        model.addAttribute("user", userMapper.findByUsername(username));
-        return "user_page";
+    @GetMapping("/{username}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable("username") String username) {
+        return ResponseEntity.status(HttpStatus.OK).body(userMapper.findByUsername(username));
     }
 }
