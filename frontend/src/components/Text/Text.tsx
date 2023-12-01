7526d541eaa6d5fd107ec1e1e712e3c8f5090418
Evaletik <vasiliy.kin1@gmail.com>
2023-12-01 20:22:10 +0100

    Added some new styled components & new components for small or tiny text

---
+++ frontend/src/components/Text/Text.tsx
@@ -0,0 +1,50 @@
+import { Box, BoxProps, styled } from "@mui/material"
+import { FC } from "react"
+
+const StyledBox = styled(Box)<{
+  textTransformStyle?: any
+  ellipsis?: boolean
+}>(({ textTransformStyle, ellipsis }) => ({
+  textTransform: textTransformStyle || "none",
+  whiteSpace: ellipsis ? "nowrap" : "normal",
+  overflow: ellipsis ? "hidden" : "",
+  textOverflow: ellipsis ? "ellipsis" : "",
+}))
+
+type Props = {
+  ellipsis?: boolean
+  textTransform?: "none" | "capitalize" | "uppercase" | "lowercase" | "initial" | "inherit"
+}
+
+export const Small: FC<BoxProps & Props> = ({ children, className, ellipsis, textTransform, ...props }) => {
+  return (
+    <StyledBox
+      textTransformStyle={textTransform}
+      ellipsis={ellipsis}
+      className={className}
+      component="small"
+      fontSize="12px"
+      fontWeight="500"
+      lineHeight="1.5"
+      {...props}
+    >
+      {children}
+    </StyledBox>
+  )
+}
+
+export const Tiny: FC<BoxProps & Props> = ({ children, className, ellipsis, textTransform, ...props }) => {
+  return (
+    <StyledBox
+      textTransformStyle={textTransform}
+      ellipsis={ellipsis}
+      className={className}
+      component="small"
+      fontSize="11px"
+      lineHeight="1.5"
+      {...props}
+    >
+      {children}
+    </StyledBox>
+  )
+}
