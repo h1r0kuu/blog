9e9643a64b3937072bf09db5546ae68ace6e9f68
h1r0kuu <abhazim.nurbat@gmail.com>
2023-12-03 14:41:05 +0100

    refactor: replace hard-coded messages with constants

--- src/main/java/com/pwgp/blog/dto/auth/RegistrationRequest.java
+++ src/main/java/com/pwgp/blog/dto/auth/RegistrationRequest.java
@@ -1,5 +1,6 @@
 package com.pwgp.blog.dto.auth;
 
+import com.pwgp.blog.constants.ErrorMessage;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,11 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class RegistrationRequest {
 
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9_-]+$", message = "Username must contain only letters, numbers, underscores, or hyphens")
+    @Size(min = 3, max = 20, message = ErrorMessage.USERNAME_LENGTH_MESSAGE)
+    @Pattern(regexp = "^[a-zA-Z0-9_-]+$", message = ErrorMessage.USERNAME_FORMAT_MESSAGE)
     private String username;
     private String password;
-    @Email(message = "Invalid email format")
+    @Email(message = ErrorMessage.EMAIL_NOT_VALID)
     private String email;
     private MultipartFile avatar;
 }
