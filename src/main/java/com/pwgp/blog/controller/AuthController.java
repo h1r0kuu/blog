567f6779999bd8149ea53c1ba40bbcdae292a962
h1r0kuu <abhazim.nurbat@gmail.com>
2023-12-07 21:50:07 +0100

    feat: integrate springdoc-openapi for API documentation

--- src/main/java/com/pwgp/blog/controller/AuthController.java
+++ src/main/java/com/pwgp/blog/controller/AuthController.java
@@ -6,6 +6,12 @@ import com.pwgp.blog.dto.auth.RegistrationRequest;
 import com.pwgp.blog.mapper.UserMapper;
 import com.pwgp.blog.service.AuthenticationService;
 import com.pwgp.blog.service.VerificationTokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,29 +25,53 @@ import static com.pwgp.blog.constants.PathConstants.*;
 @RestController
 @RequestMapping(API_V1_AUTH)
 @RequiredArgsConstructor
+@Tag(name = "Auth Controller")
 public class AuthController {
 
     private final UserMapper userMapper;
     private final VerificationTokenService tokenService;
     private final AuthenticationService authenticate;
 
+    @Operation(summary = "User Login")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful login"),
+        @ApiResponse(responseCode = "401", description = "Invalid credentials"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(LOGIN)
     public ResponseEntity<JwtResponse> login(@RequestBody AuthRequest authRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(authenticate.login(authRequest));
     }
 
+    @Operation(summary = "User Registration")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful registration"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(REGISTRATION)
     public ResponseEntity<?> register(@Valid @ModelAttribute RegistrationRequest registrationRequest) {
         userMapper.create(registrationRequest);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "Verify Registration Token")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Token verification successful"),
+        @ApiResponse(responseCode = "400", description = "Invalid token"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping(REGISTRATION_VERIFICATION_TOKEN)
     public ResponseEntity<?> verifyRegistrationToken(@PathVariable("token") String token) {
         tokenService.verify(token);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "Resend Verification Token")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Token resent successfully"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(RESEND_VERIFICATION_TOKEN)
     public String resendActivationCode() throws MessagingException {
         tokenService.resendToken();
--- src/main/java/com/pwgp/blog/controller/UserController.java
+++ src/main/java/com/pwgp/blog/controller/UserController.java
@@ -5,6 +5,12 @@ import com.pwgp.blog.dto.settings.ChangePasswordRequest;
 import com.pwgp.blog.dto.user.UserProfileResponse;
 import com.pwgp.blog.mapper.FollowMapper;
 import com.pwgp.blog.mapper.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,44 +25,85 @@ import static com.pwgp.blog.constants.PathConstants.*;
 @RestController
 @RequestMapping(API_V1_USERS)
 @RequiredArgsConstructor
+@Tag(name = "User Controller")
 public class UserController {
 
     private final UserMapper userMapper;
     private final FollowMapper followMapper;
 
+    @Operation(summary = "Get User Profile")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User profile retrieved successfully"),
+        @ApiResponse(responseCode = "404", description = "User not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping(USERNAME)
     public ResponseEntity<UserProfileResponse> getUser(@PathVariable("username") String username) {
         return ResponseEntity.status(HttpStatus.OK).body(userMapper.findByUsername(username));
     }
 
+    @Operation(summary = "Change User Password")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Password changed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PutMapping(CHANGE_PASSWORD)
     public ResponseEntity<?> changeUserPassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(userMapper.changePassword(changePasswordRequest));
     }
 
+    @Operation(summary = "Change General Settings")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "General settings changed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PutMapping(CHANGE_GENERAL)
     public ResponseEntity<?> changeGeneralSettings(@RequestBody ChangeGeneralSettingsRequest changeGeneralSettingsRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(userMapper.changeGeneralSettings(changeGeneralSettingsRequest));
     }
 
+    @Operation(summary = "Get User Followers")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User followers retrieved successfully"),
+        @ApiResponse(responseCode = "404", description = "User not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping(USER_FOLLOWERS)
     public ResponseEntity<Page<UserProfileResponse>> getUserFollowers(@PathVariable("username") String username,
                                                                @PageableDefault Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(followMapper.getUserFollowers(username, pageable));
     }
 
+    @Operation(summary = "Get User Followings")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User followings retrieved successfully"),
+        @ApiResponse(responseCode = "404", description = "User not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping(USER_FOLLOWINGS)
     public ResponseEntity<Page<UserProfileResponse>> getUserFollowings(@PathVariable("username") String username,
                                                                        @PageableDefault Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(followMapper.getUserFollowings(username, pageable));
     }
 
+    @Operation(summary = "Follow User")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User followed successfully"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping(FOLLOW_USER)
     public ResponseEntity<?> followUser(@PathVariable("username") String username) {
         followMapper.followUser(username);
         return ResponseEntity.status(HttpStatus.OK).body("dsa");
     }
 
+    @Operation(summary = "Unfollow User")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User unfollowed successfully"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @DeleteMapping(UNFOLLOW_USER)
     public ResponseEntity<?> unfollowUser(@PathVariable("username") String username) {
         followMapper.unfollowUser(username);
